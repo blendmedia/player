6badbbc6ff5e96db6046cc43040e3f35dace7c79
Ashley Blurton <ashley@blend.media>
2018-01-16 14:57:42 +0000

    Add comments to register

--- src/register.js
+++ src/register.js
@@ -1,10 +1,22 @@
 import Base from "./interfaces/Base";
 
 let _store = {};
+
+/**
+ * Clear component store
+ */
 export function reset() {
   _store = {};
 }
 
+/**
+ * Register a class prototype
+ * @param  {String}  name     Name to bind component to
+ * @param  {Function}  cls      Class function to store
+ * @param  {Boolean} [override=false] If false do not override component
+ * if already registered
+ * @return {Boolean]}           if registering was successful
+ */
 export function register(name, cls, override = false) {
   if (!(cls.prototype instanceof Base)) {
     return false;
@@ -18,12 +30,18 @@ export function register(name, cls, override = false) {
   return true;
 }
 
+/**
+ * Get a component function from a name. Function values are returned as-is
+ * @param  {String|Array<String>} name Name of the component to retrieve,
+ * or an array of names
+ * @return {Function}
+ */
 export function resolve(name) {
   if (name instanceof Array) {
     return name.map(resolve);
   }
 
-  if (typeof name !== "string") {
+  if (typeof name === "function") {
     return name;
   }
 
