f1875ced8ff684a9aee2e2194cee0a9835966d6e
Ashley Blurton <ashley@blend.media>
2018-01-19 16:14:12 +0000

    Setup source maps and add debug dependency for dev output

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,9 @@ import common from "./common";
 import resolve from "rollup-plugin-node-resolve";
 
 export default Object.assign({}, common, {
+  output: Object.assign({}, common.output, {
+    sourcemap: true,
+  }),
   plugins: [
     resolve({
       jsnext: true,
