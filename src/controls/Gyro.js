49c2a6e1009087869d3517467e375f9c5e1176ac
Ashley Blurton <ashley@blend.media>
2018-03-24 18:08:15 +0000

    Update gyro to use correct values
    Detect Chrome version and convert rad/sec to deg/sec, and disable completely in Chrome v65 only.

--- src/controls/Gyro.js
+++ src/controls/Gyro.js
@@ -1,4 +1,5 @@
-import { IS_IOS } from "../util/device";
+import { IS_CHROME, CHROME_VERSION } from "../util/device";
+import { DEGREE } from "../util/math";
 import { register } from "../register";
 import Controller from "../interfaces/Controller";
 import { DEVICE_MOTION } from "../events";
@@ -26,6 +27,8 @@ class Gyro extends Controller {
     this.on(DEVICE_MOTION, this._onMove, true);
     this._moveX = 0;
     this._moveY = 0;
+    this._speedX = 0;
+    this._speedY = 0;
   }
 
   destroy() {
@@ -33,39 +36,54 @@ class Gyro extends Controller {
   }
 
   _onMove(e) {
-    const { interval, rotationRate } = e;
+    const { rotationRate } = e;
+    if (IS_CHROME && CHROME_VERSION === 65) {
+      return;
+    }
     let { alpha, beta } = rotationRate;
     if (alpha === null || beta === null) {
       return;
     }
-    if (IS_IOS) {
-      alpha *= interval;
-      beta *= interval;
+
+    if (IS_CHROME && CHROME_VERSION < 65) {
+      alpha *= DEGREE;
+      beta *= DEGREE;
     }
+
     switch (orientation.angle) {
       case 0:
-        this._moveX = alpha;
-        this._moveY = beta;
+        this._speedX = alpha;
+        this._speedY = beta;
         break;
       case 90:
-        this._moveX = -beta;
-        this._moveY = alpha;
+        this._speedX = -beta;
+        this._speedY = alpha;
         break;
       case -90:
       case 270:
-        this._moveX = beta;
-        this._moveY = -alpha;
+        this._speedX = beta;
+        this._speedY = -alpha;
         break;
       case 180:
-        this._moveX = beta;
-        this._moveY = -alpha;
+        this._speedX = beta;
+        this._speedY = -alpha;
         break;
     }
+  }
+
+  fixedUpdate(dt) {
+    this._moveX += this._speedX * (dt / 1000);
+    this._moveY += this._speedY * (dt / 1000);
   }
 
   apply({ x, y, z}) {
     x += this._moveX;
     y += this._moveY;
+
+    this._moveX = 0;
+    this._moveY = 0;
+    this._speedX = 0;
+    this._speedY = 0;
 
     return { x, y, z };
   }
